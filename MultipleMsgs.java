42868a996c1a0843a309795289065b03e183693e
CarboNitee <111556748+CarboNitee@users.noreply.github.com>
2022-08-19 21:25:39 +0800

    Add files via upload

---
+++ MultipleMsgs.java
@@ -0,0 +1,16 @@
+public class MultipleMsgs
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "Dark Hummor is like food." );
+		System.out.println( "Not everyone gets it." );
+		System.out.println( "Why are Americans bad chess players?" );
+		System.out.println( "they lost two towers." );
+		System.out.println( "I have many jokes about unemployed people" );
+		System.out.println( "Sadly, non of them work." );
+		System.out.println( "My greif counselor died the other day" );
+        System.out.println( "He was so good at his job I didn't even care." );
+        System.out.println( "Old people in weddings always poke me and say \"You're next\" " );
+        System.out.println( "So I started doing the same to them in funerals " );
+	}
+}
---
+++ PostCard.java
@@ -0,0 +1,15 @@
+public class GoodFirstProgram
+{
+	public static void main( String[] args )
+	{
+		System.out.println( " /-------------------------------\" );
+		System.out.println( "|                          øøøøø |" );
+		System.out.println( "|   Name: Richard Shiau    øøøøø |" );
+        System.out.println( "|                          øøøøø |" );
+		System.out.println( "|   Adress: 302, Hsinchu County, |" );
+        System.out.println( "|   Zhubei City, Section 1,      |" );
+		System.out.println( "|   Fuxingsan Rd, No 212         |" );
+        System.out.println( "|                                |" );
+		System.out.println( "\--------------------------------/" );
+	}
+}
