bfe3986a7d71bf694d9f55b509844c702b1432b6
CarboNitee <111556748+CarboNitee@users.noreply.github.com>
2022-09-04 23:34:13 +0800

    Add files via upload
    Variables(2)

---
+++ Variables/Schedule.java
@@ -0,0 +1,36 @@
+public class Schedule {
+
+    public static void main( String[] args )
+    {
+        String course1, course2, course3, course4, course5, course6, course7, course8, teacher1, teacher2, teacher3, teacher4, teacher5, teacher6, teacher7, teacher8;
+
+        course1 = "English";
+        course2 = "Expository writing";
+        course3 = "Math";
+        course4 = "History";
+        course5 = "Biology";
+        course6 = "Economy";
+        course7 = "Art";
+        course8 = "Java";
+        teacher1 = "Mr. Robert";
+        teacher2 = "Mr. Raj";
+        teacher3 = "Ms. Eleanor";
+        teacher4 = "Ms. Tina";
+        teacher5 = "Mr. Ciancio";
+        teacher6 = "Mr. Cook";
+        teacher7 = "Ms. Sue";
+        teacher8 = "Mr. Bob";
+
+        System.out.println( " ============================================================" );
+        System.out.println( "║ 1 ║           "+ teacher5 +"       ║       "+ course1 +"            ║" );
+        System.out.println( "║ 2 ║           "+ teacher8 +"           ║       "+ course2 +" ║" );
+        System.out.println( "║ 3 ║           "+ teacher1 +"        ║       "+ course3 +"               ║" );
+        System.out.println( "║ 4 ║           "+ teacher6 +"          ║       "+ course4 +"            ║" );
+        System.out.println( "║ 5 ║           "+ teacher2 +"           ║       "+ course5 +"            ║" );
+        System.out.println( "║ 6 ║           "+ teacher3 +"       ║       "+ course6 +"            ║" );
+        System.out.println( "║ 7 ║           "+ teacher7 +"           ║       "+ course7 +"                ║" );
+        System.out.println( "║ 8 ║           "+ teacher4 +"           ║       "+ course8 +"               ║" );
+        System.out.println( " ============================================================" );
+
+    }
+}
---
+++ Variables/StillUsingVariables.java
@@ -0,0 +1,14 @@
+public class StillUsingVariables
+{
+    public static void main( String[] args )
+    {
+        String Name;
+        int graduationYear;
+          
+        graduationYear = 2025;
+        Name = "Richard Shiau";
+
+        System.out.println( "My name is " +Name+ " and I will graduate in the year " +graduationYear+ "." );
+    }
+}
+
